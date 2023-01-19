b9a6a6bf9210480edbf700bbe053be22c57811a3
pidzhak <pidzhak84@gmail.com>
2023-01-19 23:07:57 +0300

    Решение Задачи 2-12

---
+++ Ex_2_12/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Вход: два числа.
+Вывод: является ли второе число кратным первому. Если число 2 кратно числу 1, то программа выводит остаток от деления.
+(34, 5 -> не кратно, остаток 4; 16, 4 -> кратно)
+*/
+
+Console.Clear();
+
+Console.Write("Введите number1 - ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите number2 - ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    Console.Write("кратно");
+}
+else
+{
+    int rem = number1 % number2;
+    Console.Write($"не кратно, остаток от деления {rem} ");
+}
